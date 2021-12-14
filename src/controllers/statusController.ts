5d112d289d48ccfe876f49b40b487ca8eda15f0c
Angélica Cavalheiro Rodrigues <angelicacavalheiro51@gmail.com>
2021-12-14 14:59:47 -0300

    Routers testing

---
+++ src/controllers/statusController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express';
+
+export async function statusController(req: Request, res: Response) {
+    res.send('server online')
+}
+
+export async function statusController1(req: Request, res: Response) {
+    res.send('server online rota 1')
+}
---
+++ src/routers/statusRouter.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import * as controller from '../controllers/statusController'
+
+const router = Router();
+
+
+router.get('', controller.statusController);
+router.get('/1', controller.statusController1);
+
+export default router;
