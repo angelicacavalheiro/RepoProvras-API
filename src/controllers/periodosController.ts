7fcbe3bfdd1332146fae0e044a2827ce62335264
Nivea <nivea@respondeai.com.br>
2022-01-04 08:52:36 -0300

    preparando para o review

--- src/controllers/periodosController.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-import Periodo from '../entities/Periodo';
-
-export async function findAllSemesters (req: Request, res: Response) {
-    const result = await getRepository(Periodo).find();
-    res.send(result);
-}
--- src/routers/statusRouter.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import * as controller from '../controllers/statusController'
-
-const router = Router();
-
-router.get('', controller.statusController);
-router.get('/1', controller.statusController1);
-
-export default router;
